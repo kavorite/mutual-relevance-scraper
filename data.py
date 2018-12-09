c646b0f65ad9897b400242afb5469b715ec5709c
алый <iseurie@gmail.com>
2018-12-09 10:28:45 -0500

    silly ref bug

--- data.py
+++ data.py
@@ -151,7 +151,7 @@ if __name__ == '__main__':
         spinner_index %= len(SPINNER)
 
     startbytes = 0
-    ostream = stdout.buffer if parsed.opath is None else open(opath, 'w+')
+    ostream = stdout.buffer if parsed.opath is None else open(parsed.opath, 'w+')
     with ostream as o:
         if o is not stdout.buffer:
             o.seek(0, SEEK_END)
